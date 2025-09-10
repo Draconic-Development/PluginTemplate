2f43e0af9c5218ae93b7290dc2cf3b7e58c78348
Mike3132 <mspicer32@gmail.com>
2025-09-10 18:44:05 -0400

    testing

--- src/main/java/dev/draconicdevelopment/pluginTemplate/commands/ExampleCommand.java
+++ src/main/java/dev/draconicdevelopment/pluginTemplate/commands/ExampleCommand.java
@@ -11,7 +11,7 @@ import org.bukkit.entity.Player;
 public class ExampleCommand implements CommandExecutor {
 
     private final PluginTemplate plugin;
-    // adsfdasf
+    // ssss
 
     //TODO: Change this executor to match the command used
     public ExampleCommand(PluginTemplate plugin) {
